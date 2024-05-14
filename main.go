92b13728b442ae3543517974fc694b21a35e167d
Shaxboz Vaxobov <154572503+akramov1ch@users.noreply.github.com>
2024-05-14 07:55:06 +0500

    Update main.go

--- main.go
+++ main.go
@@ -4,23 +4,16 @@ import (
     "fmt"
     "net/url"
 )
-
-func Check_Url(URL string) bool {
-    _, err := url.ParseRequestURI(URL)
-    return err == nil
-}
-
 func main() {
     true_url := "https://example.com/path/to/resource?param1=value1&param2=value2#fragment"
     false_url := "http://example.com/path/to/resource?param1=value1&param2=value2#fragment#"
 
-	if Check_Url(true_url) {
+    if _, err := url.ParseRequestURI(true_url) && err{
         fmt.Println("This URL is true", true_url)
     } else {
         fmt.Println("This URL is false", true_url)
     }
-
-	if Check_Url(false_url) {
+    if _, err := url.ParseRequestURI(false_url) && err {
         fmt.Println("This URL is true", false_url)
     } else {
         fmt.Println("This URL is false", false_url)
